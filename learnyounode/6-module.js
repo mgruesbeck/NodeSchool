dcbdacfa777219dbe9a5ffca2c352183b8b11ec3
mgruesbeck <mgruesbeck@gmail.com>
2015-12-17 17:42:54 -0700

    finish modular lesson

--- learnyounode/6-module.js
+++ learnyounode/6-module.js
@@ -1,3 +1,20 @@
+var fs   = require('fs');
+var path = require('path');
+
+module.exports = function(dirPath, filterString, callback) {
+    fs.readdir(dirPath, function (err, data) {
+        if(err) {
+            return callback(err);
+        }
+        data.forEach(function (file) {
+            if (path.extname(file) === '.' + filterString) {
+                callback(null, file);
+            }
+        })
+    })
+}
+
+/*
 module.exports = {
     englishHello : function() {
         return('hello');
@@ -6,3 +23,4 @@ module.exports = {
         return('hola');
     }
 }
+*/
--- learnyounode/6-origin.js
+++ learnyounode/6-origin.js
@@ -1,4 +1,14 @@
 var module = require('./6-module.js');
+module(process.argv[2], process.argv[3], function(err, data){ 
+    if(err){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
 
+
+/*
 console.log(module.englishHello());
 console.log(module.spanishHello());
+*/
