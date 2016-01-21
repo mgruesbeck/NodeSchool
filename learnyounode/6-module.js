2f6c2ccff84df9f2a08b0b057f515b972a752fbc
mgruesbeck <mgruesbeck@gmail.com>
2016-01-21 15:38:16 -0800

    finish make it modular

--- learnyounode/6-module.js
+++ learnyounode/6-module.js
@@ -1,26 +1,42 @@
 var fs   = require('fs');
 var path = require('path');
 
-module.exports = function(dirPath, filterString, callback) {
-    fs.readdir(dirPath, function (err, data) {
-        if(err) {
-            return callback(err);
-        }
-        data.forEach(function (file) {
-            if (path.extname(file) === '.' + filterString) {
-                callback(null, file);
-            }
+module.exports = function (dir, filterStr, callback) {
+
+    fs.readdir(dir, function (err, list) {
+        if (err)
+        return callback(err)
+
+        list = list.filter(function (file) {
+            return path.extname(file) === '.' + filterStr
         })
+
+    callback(null, list)
     })
 }
 
-/*
-module.exports = {
-    englishHello : function() {
-        return('hello');
-    },
-    spanishHello : function() {
-        return('hola');
-    }
-}
-*/
+/* second try at task 
+   module.exports = function(dirPath, filterString, callback) {
+   fs.readdir(dirPath, function (err, data) {
+   if(err) {
+   return callback(err);
+   }
+   data.forEach(function (file) {
+   if (path.extname(file) === '.' + filterString) {
+   callback(null, file);
+   }
+   })
+   })
+   }
+   */
+
+/* early test linking files
+   module.exports = {
+   englishHello : function() {
+   return('hello');
+   },
+   spanishHello : function() {
+   return('hola');
+   }
+   }
+   */
--- learnyounode/6-origin.js
+++ learnyounode/6-origin.js
@@ -1,14 +1,27 @@
-var module = require('./6-module.js');
-module(process.argv[2], process.argv[3], function(err, data){ 
-    if(err){
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
+var filterFn = require('./6-module.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+filterFn(dir, filterStr, function (err, list) {
+    if (err)
+    return console.error('There was an error:', err)
+
+    list.forEach(function (file) {
+        console.log(file)
+    })
+})
+
+/* second try at task
+   module(process.argv[2], process.argv[3], function(err, data){ 
+   if(err){
+   console.log(err);
+   } else {
+   console.log(data);
+   }
+   });
 
 
-/*
+/* early test linking files
 console.log(module.englishHello());
 console.log(module.spanishHello());
 */
